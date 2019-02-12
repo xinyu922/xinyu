35dc202085a2d9e01a1c3c333c469c4c4fba23b1
xinyu922 <17073549084@163.com>
2019-02-12 11:01:22 +0800

    add c++ origin code 20190212

---
+++ GCC_pre_c++11/1/cmake-build-debug/CMakeFiles/add_item.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add_item.dir/add_item.cc.o"
+  "add_item.pdb"
+  "add_item"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add_item.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GCC_pre_c++11/5/cmake-build-debug/CMakeFiles/add_item2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add_item2.dir/add_item2.cc.o"
+  "add_item2.pdb"
+  "add_item2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add_item2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
