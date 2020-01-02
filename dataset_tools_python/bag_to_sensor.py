06855a19ecbcb396b0a7d5372a820cabec1334a2
xinyu <17073549084@163.com>
2020-01-02 16:58:27 +0800

    add code for get data from rosbag(image and imu), xinyu922, 20200102

---
+++ dataset_tools_python/bag_to_sensor.py
@@ -0,0 +1,112 @@
+# coding:utf-8
+# !/usr/bin/python
+
+# Extract images from a bag file.
+
+# PKG = 'beginner_tutorials'
+import roslib;  # roslib.load_manifest(PKG)
+import rosbag
+import rospy
+import cv2
+import os
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from cv_bridge import CvBridgeError
+
+import sys
+
+reload(sys)
+
+sys.setdefaultencoding('utf8')
+
+rosbag_file = '/media/dzsb078/D82A47592A4733B2/00_Dataset/无人机_数据集/20191112/5/2019-11-12-17-04-56.bag'
+data_save_path = '/media/dzsb078/D82A47592A4733B2/00_Dataset/无人机_数据集/20191112/5/无人机_20191112_data_set5/'
+
+
+class GetSensorData():
+    def __init__(self):
+        self.mkdir()
+        self.get_image_left()
+        self.get_image_right()
+        self.get_imu()
+
+    def mkdir(self):
+
+        folder_images = os.path.exists(data_save_path + 'images')
+        folder_imu = os.path.exists(data_save_path + 'imu')
+
+
+        folder_images_left_path = data_save_path + 'images/left'
+        folder_images_right_path = data_save_path + 'images/right'
+        folder_imu_path = data_save_path + 'imu'
+
+        # folder_images = os.path.exists(data_save_path + 'images')
+        if not folder_images and not folder_imu:
+            os.makedirs(folder_images_left_path)
+            os.makedirs(folder_images_right_path)
+            os.makedirs(folder_imu_path)
+        else:
+            pass
+
+
+    def get_image_left(self):
+        self.bridge = CvBridge()
+        with rosbag.Bag(rosbag_file, 'r') as bag:  # 要读取的bag文件；
+            for topic, msg, t in bag.read_messages():
+                if topic == "/image_raw1":  # 图像的topic；
+                    try:
+                        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                    except CvBridgeError as e:
+                        print(e)
+                    # timestr = "%.6f" %  msg.header.stamp.to_sec()
+                    timestr = msg.header.stamp.to_nsec()
+
+                    # %.6f表示小数点后带有6位，可根据精确度需要修改；
+                    image_name = str(timestr) + ".png"  # 图像命名：时间戳.png
+                    left_path = data_save_path + 'images/left/'
+                    cv2.imwrite(left_path + image_name, cv_image)  # 保存；
+
+    def get_image_right(self):
+        self.bridge = CvBridge()
+        with rosbag.Bag(rosbag_file, 'r') as bag:  # 要读取的bag文件；
+            for topic, msg, t in bag.read_messages():
+                if topic == "/image_raw2":  # 图像的topic；
+                    try:
+                        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                    except CvBridgeError as e:
+                        print(e)
+                    timestr = msg.header.stamp.to_nsec()
+                    # %.6f表示小数点后带有6位，可根据精确度需要修改；
+                    image_name = str(timestr) + ".png"  # 图像命名：时间戳.png
+                    right_path = data_save_path + 'images/right/'
+                    cv2.imwrite(right_path + image_name, cv_image)  # 保存；
+
+    def get_imu(self):
+        with rosbag.Bag(rosbag_file, 'r') as bag:  # 要读取的bag文件；
+            imu_path = data_save_path + 'imu/imu.txt'
+            f_imu = open(imu_path, 'w')
+            f_imu.write('# timestamp ang_v_x ang_v_y ang_v_z lin_acc_x lin_acc_y lin_acc_z qx qy qz qw\n')
+            for topic, msg, t in bag.read_messages():
+                if topic == "/imu/data": #图像的topic；
+                    f_imu.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % (
+                             msg.header.stamp.to_sec(),
+                             msg.angular_velocity.x,
+                             msg.angular_velocity.y,
+                             msg.angular_velocity.z,
+
+                             msg.linear_acceleration.x,
+                             msg.linear_acceleration.y,
+                             msg.linear_acceleration.z,
+
+                             msg.orientation.x,
+                             msg.orientation.y,
+                             msg.orientation.z,
+                             msg.orientation.w))
+            f_imu.close()
+
+
+if __name__ == '__main__':
+    try:
+        getsensordata = GetSensorData()
+    except rospy.ROSInterruptException:
+        pass
